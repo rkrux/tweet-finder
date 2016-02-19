7fc2131033a88c285f8a2a9614bfc3b11ee7fdc4
Rushil <rushilkapoor29@gmail.com>
2016-02-19 19:09:58 +0530

    Refactored code to remove return statements

--- TwitterApi.php
+++ TwitterApi.php
@@ -9,20 +9,25 @@ class TwitterApi {
     private $consumer_secret;
     private $oauth_access_token;
     private $oauth_access_token_secret;
+
     private $base_url;
     private $request_method;
     private $query;
     
+    private $base_string;
+    private $oauth;
+    private $auth_header;
+
     /**
      * Creates the API object and initializes the variables.
      *
-     * @param array $credentials    Set of credentials of the user
+     * @param array $credentials Set of credentials of the user
      * 
      * @throws Exception When cURL isn't installed or incorrect credentials are sent
      */
     public function __construct($credentials) {
         if(!in_array('curl', get_loaded_extensions())){
-            throw new Exception("Install curl to run the application.");
+            throw new Exception("Install cURL and its PHP extension to run the application.");
         }
         if (!isset($credentials['consumerKey']) || !isset($credentials['consumerSecret'])
             || !isset($credentials['oauthAccessToken']) || !isset($credentials['oauthAccessTokenSecret'])) {
@@ -37,8 +42,8 @@ class TwitterApi {
     /**
      * Initializes other variables. Subject to change for every request call.
      *
-     * @param string $base_url  Base url of API request
-     * @param string $request_method    Type of API request
+     * @param string $base_url Base url of API request
+     * @param string $request_method Type of API request
      * @param string $query Query parameters of the request
      */
     private function initializeParameters($base_url, $request_method, $query) {
@@ -50,9 +55,9 @@ class TwitterApi {
     /**
      * Creates base string for Oauth signature
      * 
-     * @param string $parameters    Associative array of keys and values of request  
+     * @param string $parameters Associative array of keys and values of request  
      * 
-     * @return string $base   Base string 
+     * @return string $base Base string 
      */
     private function createBaseString($parameters) {
         $key_val = array();
@@ -60,8 +65,7 @@ class TwitterApi {
         foreach($parameters as $key => $value){
             $key_val[] = rawurlencode($key) . '=' . rawurlencode($value);
         }
-        $base = ($this->request_method . '&' . rawurlencode($this->base_url) . '&' . rawurlencode(implode('&', $key_val)));
-        return $base;
+        $this->base_string = ($this->request_method . '&' . rawurlencode($this->base_url) . '&' . rawurlencode(implode('&', $key_val)));
     }
     
     /**
@@ -71,7 +75,7 @@ class TwitterApi {
      * @return array $oauth
      */
     private function createOauth() {
-        $oauth = array(
+        $this->oauth = array(
             'oauth_consumer_key' => $this->consumer_key,
             'oauth_nonce' => time(),
             'oauth_signature_method' => 'HMAC-SHA1',
@@ -81,45 +85,43 @@ class TwitterApi {
         );
         $query_trim = str_replace('?', '', $this->query);
         $query_array = explode('=', $query_trim);
-        $oauth[$query_array[0]] = $query_array[1];
+        $this->oauth[$query_array[0]] = $query_array[1];
         
-        $base_string = $this->createBaseString($oauth);
+        $this->createBaseString($this->oauth);
         $secret_key = rawurlencode($this->consumer_secret) . '&' . rawurlencode($this->oauth_access_token_secret);
-        $oauth_signature = base64_encode(hash_hmac('sha1', $base_string, $secret_key, true));
-        $oauth['oauth_signature'] = $oauth_signature;
-        return $oauth;
+        $oauth_signature = base64_encode(hash_hmac('sha1', $this->base_string, $secret_key, true));
+        $this->oauth['oauth_signature'] = $oauth_signature;
     }
     
     /**
-     * Created Authorization header used bu cURL
+     * Created Authorization header used by cURL
      * 
-     * @return string $auth_header  Comma separated header used in cURL request   
+     * @return string $auth_header Comma separated header used in cURL request   
      */
     private function createAuthHeader() {     
-        $oauth = $this->createOauth();
-        $auth_header = 'Authorization: OAuth ';
+        $this->createOauth();
+        $this->auth_header = 'Authorization: OAuth ';
         $values = array();
-        foreach($oauth as $key => $value) {
+        foreach($this->oauth as $key => $value) {
             if (in_array($key, array('oauth_consumer_key', 'oauth_nonce', 'oauth_signature',
                 'oauth_signature_method', 'oauth_timestamp', 'oauth_token', 'oauth_version'))) {
                 $values[] = "$key=\"" . rawurlencode($value) . "\"";
             }
         }
-        $auth_header .= implode(', ', $values);
-        return $auth_header;
+        $this->auth_header .= implode(', ', $values);
     }
     
     /**
      * Executes the actual request to Twitter API and receives JSON data
      *
-     * @throws Exception    When an error occurs during cURL reuqest
+     * @throws Exception When an error occurs during cURL reuqest
      * 
-     * @return string $result   JSON response from Twitter API
+     * @return string $result JSON response from Twitter API
      */
     private function executeRequest() {
-        $auth_header = $this->createAuthHeader();
+        $this->createAuthHeader();
         $query_array = explode('=', $this->query);
-        $header = array($auth_header, 'Expect:');
+        $header = array($this->auth_header, 'Expect:');
         $options = array( CURLOPT_HTTPHEADER => $header,
                       CURLOPT_HEADER => false,
                       CURLOPT_URL => ($this->base_url . $query_array[0] . '=' . urlencode($query_array[1])),
@@ -139,7 +141,7 @@ class TwitterApi {
     /**
      * Displays the tweets satisfying the conditions of retweeted atlest once
      * 
-     * @param array   $result   JSON encoded response from the Twitter API
+     * @param array $result JSON encoded response from the Twitter API
      */
     private function filterResult($result) {
         $decode = json_decode($result, true);
@@ -164,8 +166,8 @@ class TwitterApi {
     /**
      * Public function called from the class object. Calls other private functions to perform the task
      * 
-     * @param string $base_url  Base url of API request
-     * @param string $request_method    Type of API request
+     * @param string $base_url Base url of API request
+     * @param string $request_method Type of API request
      * @param string $query Query parameters of the request
      */
     public function makeRequest($base_url, $request_method, $query) {
